b6bff7486820ae4787d95fcaa6e0b1b7d81bf466
robot <yunseojun2@naver.com>
2022-05-30 14:07:29 +0900

    test

--- app/src/main/java/com/example/mylandmark/GuideActivity.java
+++ app/src/main/java/com/example/mylandmark/GuideActivity.java
@@ -2,7 +2,11 @@ package com.example.mylandmark;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class GuideActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class GuideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+
+        findViewById(R.id.img_map).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(GuideActivity.this, "이미지 클릭", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("geo:37.5111158,127.098167?z=16"));
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mylandmark/infoActivity.java
+++ app/src/main/java/com/example/mylandmark/infoActivity.java
@@ -3,18 +3,21 @@ package com.example.mylandmark;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class infoActivity extends AppCompatActivity {
+public class infoActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView img_info;
     TextView  txt_info;
+    Button    btn_info_back;
     int imgno = 0;
-    int imgs[] = {R.drawable.info1 , R.drawable.info2,
-             R.drawable.info3,R.drawable.info4,
-             R.drawable.info5   };
-    String txts[] = {"1번 설명","2번 설명","3번 설명","4번 설명"};
+    int imgs[] = {R.drawable.img_1 , R.drawable.img_2,
+             R.drawable.img_3,R.drawable.img_4,
+             R.drawable.img_5 };
+    String txts[] = {"레고랜드 소개","짜릿한 어트랙션","온가족이 함께하는 놀이기구","레고와 함께하는 레고호텔","전세계 레고가 있는 레고 상점"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +26,24 @@ public class infoActivity extends AppCompatActivity {
 
         img_info = findViewById(R.id.image_info);
         txt_info = findViewById(R.id.txt_info);
+        btn_info_back = findViewById(R.id.btn_info_back);
+
+        img_info.setImageResource(imgs[imgno]);
+        txt_info.setText(txts[imgno]);
+
+        img_info.setOnClickListener(this);
+        btn_info_back.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_info_back) {
+
+            finish();
+        }else if(view.getId() == R.id.image_info){
+            imgno = ++imgno % 5;
+            img_info.setImageResource(imgs[imgno]);
+            txt_info.setText(txts[imgno]);
+        }
     }
 }
